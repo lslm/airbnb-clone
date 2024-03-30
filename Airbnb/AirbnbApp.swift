834d395e2fccaa2b03b6d08bcdbb8d728228e586
Lucas Santos <lucas.santos.silva2011@gmail.com>
2024-03-30 13:52:06 -0300

    Initial Commit

---
+++ Airbnb/AirbnbApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbApp.swift
+//  Airbnb
+//
+//  Created by Lucas Santos on 30/03/24.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
